13e67bfbb0f07aea5845d035e3d204ce9e49de9d
Дмитрий Иванов <randomsize@narod.ru>
2022-08-09 22:56:40 +0400

    Add project files.

---
+++ mantis-tests/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF;
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'rT3kBPW8w51A7MWWs/B/8Fbb+GqLLD7Pq5H5bpcRkn0=';
---
+++ mantis-tests/tests/AccountCreationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Linq;
+
+namespace mantis_tests
+{
+
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [TestFixtureSetUp]
+
+        public void setUpConfig()
+        {
+            app.Ftp.BackupFile("/config_inc.php");
+            using (Stream localfile = File.Open("config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("/config_inc.php", localfile);
+            }
+            
+        }
+        
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData()
+            {
+                Name = "testuser",
+                Password = "password",
+                Email = "testuser@localhost.localdomain"
+            };
+
+            app.Registration.Register(account);
+        }
+        [TestFixtureTearDown]
+        public void RestoreConfig()
+        {
+            app.Ftp.RestoreBackupFile("/config_inc.php");
+        }
+    }
+}
+    
